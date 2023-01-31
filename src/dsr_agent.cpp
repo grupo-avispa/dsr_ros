39e5c215d5d7a35c49152fd53ab712dcfebcfb16
Alberto Tudela <ajtudela@gmail.com>
2023-01-31 12:29:29 +0100

    Update agent

--- src/dsr_agent.cpp
+++ src/dsr_agent.cpp
@@ -17,8 +17,8 @@
 dsrAgent::dsrAgent(): Node("dsr_agent"), count_(0), agent_name_("agent2"), agent_id_(32){
 	// Create graph
 	G_ = std::make_shared<DSR::DSRGraph>(0, agent_name_, agent_id_, "/home/alberto/initial_dsr.json");
-	/*G_ = std::make_shared<DSR::DSRGraph>(0, agent_name_, agent_id_, "");
-	RCLCPP_INFO(this->get_logger(), "Graph loaded");
+	//G_ = std::make_shared<DSR::DSRGraph>(0, agent_name_, agent_id_, "");
+	RCLCPP_INFO(this->get_logger(), "Graph loaded2");
 	auto world_node = G_->get_node("world");
 	if(!world_node.has_value()){
 		RCLCPP_ERROR(this->get_logger(), "World node not found");
@@ -30,7 +30,7 @@ dsrAgent::dsrAgent(): Node("dsr_agent"), count_(0), agent_name_("agent2"), agent
 	}
 	DSR::Node robot_node = DSR::Node::create<robot_node_type>("robot");
 	G_->insert_node(robot_node);
-	G_->write_to_json_file("/home/alberto/initial_dsr2.json");*/
+	G_->write_to_json_file("/home/alberto/initial_dsr2.json");
 
 	// DSR graph viewer
 	//graph_viewer_ = std::make_unique<DSR::DSRViewer>(this, G_, 0, DSR::DSRViewer::view::none);
@@ -41,12 +41,12 @@ dsrAgent::~dsrAgent() {
 }
 
 void dsrAgent::timer_callback(){
-	/*// Modify the attributes of the node
+	// Modify the attributes of the node
 	auto robot_node = G_->get_node("robot");
 	G_->add_or_modify_attrib_local<level_att>(robot_node.value(), count_++);
 	// Update the node in the graph to set the modified attributes available
 	G_->update_node(robot_node.value());
-	RCLCPP_INFO(this->get_logger(), "Robot node updated with level: %d", count_);*/
+	RCLCPP_INFO(this->get_logger(), "Robot node updated with level: %d", count_);
 }
 
 int main(int argc, char** argv){
