946175e31f7cc8ea6f32319b3c61659259923aa5
Alberto Tudela <ajtudela@gmail.com>
2023-02-08 14:38:59 +0100

    Fix typos

--- include/dsr_agent/dsr_agent.hpp
+++ include/dsr_agent/dsr_agent.hpp
@@ -43,12 +43,13 @@ class dsrAgent: public rclcpp::Node{
 
 		void get_params();
 		void serial_callback(const std::shared_ptr<rclcpp::SerializedMessage> msg);
+		// TODO: Remove this function
 		template <typename R, typename D> void deserialize_and_update(
 			const std::shared_ptr<rclcpp::SerializedMessage> msg, const std::string &node_name);
 		template <typename T> void create_and_insert_node(const std::string &name);
-		template <typename T> modify_node_attributes_and_update(const std::optional<DSR::Node> &node, 
+		template <typename T> void modify_node_attributes_and_update(std::optional<DSR::Node> node, 
 																const T &msg);
-		void modify_battery_attributes_and_update(const std::optional<DSR::Node> &node, 
+		void modify_battery_attributes_and_update(std::optional<DSR::Node> node, 
 												const sensor_msgs::msg::BatteryState &msg);
 };
 
--- src/dsr_agent.cpp
+++ src/dsr_agent.cpp
@@ -18,6 +18,7 @@
 #include "sensor_msgs/msg/battery_state.hpp"
 
 #include "dsr_agent/dsr_agent.hpp"
+#include "dsr_agent/ros_attr_name.hpp"
 
 /* Initialize the publishers and subscribers */
 dsrAgent::dsrAgent(): Node("dsr_agent"){
@@ -25,7 +26,20 @@ dsrAgent::dsrAgent(): Node("dsr_agent"){
 	get_params();
 
 	// Create graph
-	G_ = std::make_shared<DSR::DSRGraph>(0, agent_name_, agent_id_, "");
+	//G_ = std::make_shared<DSR::DSRGraph>(0, agent_name_, agent_id_, "");
+
+	//! This os only for testing
+	G_ = std::make_shared<DSR::DSRGraph>(0, agent_name_, agent_id_, "/home/alberto/initial_dsr.json");
+	RCLCPP_INFO(this->get_logger(), "Graph loaded");
+	auto world_node = G_->get_node("world");
+	if(!world_node.has_value()){
+		RCLCPP_ERROR(this->get_logger(), "World node not found");
+		auto world_node = DSR::Node::create<world_node_type>("world");
+		auto id = G_->insert_node(world_node);
+		RCLCPP_INFO(this->get_logger(), "World node created with id: %d", id.value());
+	}else{
+		G_->update_node(world_node.value());
+	}
 
 	// Subscriber to the topic with a generic subscription
 	auto data = rclcpp::Node::get_topic_names_and_types();
@@ -34,7 +48,7 @@ dsrAgent::dsrAgent(): Node("dsr_agent"){
 						ros_topic_, 
 						type, 
 						rclcpp::QoS(rclcpp::SystemDefaultsQoS()),
-						std::bind(&MinimalPublisher::serial_callback, this, std::placeholders::_1)
+						std::bind(&dsrAgent::serial_callback, this, std::placeholders::_1)
 						);
 	}
 }
@@ -67,7 +81,7 @@ void dsrAgent::get_params(){
 							rcl_interfaces::msg::ParameterDescriptor()
 							.set__description("The ROS topic to subscribe to"));
 	this->get_parameter("ros_topic", ros_topic_);
-	RCLCPP_INFO(this->get_logger(), "The parameter ros_topic is set to: [%s]", ros_topic.c_str());
+	RCLCPP_INFO(this->get_logger(), "The parameter ros_topic is set to: [%s]", ros_topic_.c_str());
 
 	// DSR parameters
 	nav2_util::declare_parameter_if_not_declared(this, "dsr_node_name", rclcpp::ParameterValue(""), 
@@ -83,7 +97,7 @@ void dsrAgent::serial_callback(const std::shared_ptr<rclcpp::SerializedMessage> 
 	auto data = rclcpp::Node::get_topic_names_and_types();
 	const std::string topic_type = data[ros_topic_][0];
 	RCLCPP_INFO_ONCE(this->get_logger(), "Subscribed to topic [%s] of type [%s]", 
-						ros_topic_, topic_type.c_str());
+						ros_topic_.c_str(), topic_type.c_str());
 
 	if (topic_type == "sensor_msgs/msg/BatteryState"){
 		// Create a ROS2 message of type BatteryState
@@ -92,19 +106,16 @@ void dsrAgent::serial_callback(const std::shared_ptr<rclcpp::SerializedMessage> 
 		serializer.deserialize_message(msg.get(), &battery_msg);
 		// Get the node from the graph
 		if (auto dsr_node = G_->get_node(dsr_node_name_); dsr_node.has_value()){
-			//modify_battery_attributes_and_update(dsr_node, battery_msg);
-			modify_node_attributes_and_update<sensor_msgs::msg::BatteryState>(dsr_node, battery_msg);
+			modify_battery_attributes_and_update(dsr_node, battery_msg);
+			//modify_node_attributes_and_update<sensor_msgs::msg::BatteryState>(dsr_node, battery_msg);
 		}else{
 			create_and_insert_node<battery_node_type>(dsr_node_name_);
 		}
 
-		// Using template
+		// Using template (Optional)
 		//deserialize_and_update<sensor_msgs::msg::BatteryState, battery_node_type>(msg, dsr_node_name_);
 	}else if (topic_type == "std_msgs/msg/String"){
 		// Create a ROS2 message of type String
-		std_msgs::msg::String string_msg;
-		auto serializer = rclcpp::Serialization<std_msgs::msg::String>();
-		serializer.deserialize_message(msg.get(), &string_msg);
 		// TODO: Publish to DSR
 	}else{
 		RCLCPP_WARN_ONCE(this->get_logger(), "Received message of type [%s]. Unknown for the DSR.", topic_type.c_str());
@@ -120,6 +131,7 @@ void dsrAgent::deserialize_and_update(const std::shared_ptr<rclcpp::SerializedMe
 	// Get the node from the graph
 	if (auto dsr_node = G_->get_node(node_name); dsr_node.has_value()){
 		modify_battery_attributes_and_update(dsr_node, ros_msg);
+		//modify_node_attributes_and_update<sensor_msgs::msg::BatteryState>(dsr_node, battery_msg);
 	}else{
 		create_and_insert_node<D>(node_name);
 	}
@@ -127,13 +139,13 @@ void dsrAgent::deserialize_and_update(const std::shared_ptr<rclcpp::SerializedMe
 
 template <typename T> 
 void dsrAgent::create_and_insert_node(const std::string &name){
-	RCLCPP_ERROR(this->get_logger(), "%s node not found", name);
+	RCLCPP_ERROR(this->get_logger(), "%s node not found", name.c_str());
 	auto new_dsr_node = DSR::Node::create<T>(name);
 	auto id = G_->insert_node(new_dsr_node);
 	RCLCPP_INFO(this->get_logger(), "%s node created with id [%d]", id.value());
 }
 
-void dsrAgent::modify_battery_attributes_and_update(const std::optional<DSR::Node> &node, 
+void dsrAgent::modify_battery_attributes_and_update(std::optional<DSR::Node> node, 
 												const sensor_msgs::msg::BatteryState &msg){
 	// Modify the attributes of the node
 	G_->add_or_modify_attrib_local<battery_voltage_att>(node.value(), msg.voltage);
@@ -143,9 +155,9 @@ void dsrAgent::modify_battery_attributes_and_update(const std::optional<DSR::Nod
 	G_->add_or_modify_attrib_local<battery_capacity_att>(node.value(), msg.capacity);
 	G_->add_or_modify_attrib_local<battery_design_capacity_att>(node.value(), msg.design_capacity);
 	G_->add_or_modify_attrib_local<battery_percentage_att>(node.value(), msg.percentage);
-	G_->add_or_modify_attrib_local<battery_power_supply_status_att>(node.value(), msg.power_supply_status);
-	G_->add_or_modify_attrib_local<battery_power_supply_health_att>(node.value(), msg.power_supply_health);
-	G_->add_or_modify_attrib_local<battery_power_supply_technology_att>(node.value(), msg.power_supply_technology);
+	G_->add_or_modify_attrib_local<battery_power_supply_status_att>(node.value(), static_cast<int>(msg.power_supply_status));
+	G_->add_or_modify_attrib_local<battery_power_supply_health_att>(node.value(), static_cast<int>(msg.power_supply_health));
+	G_->add_or_modify_attrib_local<battery_power_supply_technology_att>(node.value(), static_cast<int>(msg.power_supply_technology));
 	G_->add_or_modify_attrib_local<battery_present_att>(node.value(), msg.present);
 	G_->add_or_modify_attrib_local<battery_cell_voltage_att>(node.value(), msg.cell_voltage);
 	G_->add_or_modify_attrib_local<battery_cell_temperature_att>(node.value(), msg.cell_temperature);
@@ -153,12 +165,12 @@ void dsrAgent::modify_battery_attributes_and_update(const std::optional<DSR::Nod
 	G_->add_or_modify_attrib_local<battery_serial_number_att>(node.value(), msg.serial_number);
 	// Update the node in the graph to set the modified attributes available
 	G_->update_node(node.value());
-	RCLCPP_INFO(this->get_logger(), "%s node updated", node..value().name());
+	RCLCPP_INFO(this->get_logger(), "%s node updated", node.value().name());
 }
 
 template <> 
-void dsr_agent::modify_node_attributes_and_update<sensor_msgs::msg::BatteryState>(
-	const std::optional<DSR::Node> &node, const sensor_msgs::msg::BatteryState &msg){
+void dsrAgent::modify_node_attributes_and_update<sensor_msgs::msg::BatteryState>(
+	std::optional<DSR::Node> node, const sensor_msgs::msg::BatteryState &msg){
 
 }
 
