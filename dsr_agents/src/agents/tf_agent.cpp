e7323334ef89f6487d07bde9e81755ee01777b27
Alberto Tudela <ajtudela@gmail.com>
2023-11-03 10:19:39 +0100

    Sort TF before publish

--- dsr_agents/src/agents/tf_agent.cpp
+++ dsr_agents/src/agents/tf_agent.cpp
@@ -20,10 +20,10 @@
 tfAgent::tfAgent(): AgentNode("tf_agent"){
 	// Subscriber to the tf topics
 	auto latched_profile = rclcpp::QoS(rclcpp::KeepLast(1)).transient_local().reliable();
-	tf_sub_ = this->create_subscription<tf2_msgs::msg::TFMessage>(
+	/*tf_sub_ = this->create_subscription<tf2_msgs::msg::TFMessage>(
 		"/tf", 
 		rclcpp::QoS(rclcpp::SystemDefaultsQoS()), 
-		std::bind(&tfAgent::tf_callback, this, std::placeholders::_1));
+		std::bind(&tfAgent::tf_callback, this, std::placeholders::_1));*/
 	tf_static_sub_ = this->create_subscription<tf2_msgs::msg::TFMessage>(
 		"/tf_static", 
 		latched_profile,
@@ -31,19 +31,41 @@ tfAgent::tfAgent(): AgentNode("tf_agent"){
 }
 
 void tfAgent::tf_callback(const tf2_msgs::msg::TFMessage::SharedPtr msg){
-	// Iterate over the transforms
-	for (auto trf : msg->transforms){
+	tf2_msgs::msg::TFMessage unsorted_trf, sorted_trf;
+	unsorted_trf = *msg;
+
+	// Sort the transforms
+	int i = 0;
+	std::string parent_frame = unsorted_trf.transforms[0].header.frame_id;
+	do {
+		// Copy the transforms from the unsorted vector to the sorted vector if the parent frame is the same
+		std::copy_if(unsorted_trf.transforms.begin(), unsorted_trf.transforms.end(), std::back_inserter(sorted_trf.transforms),
+						[parent_frame](const auto & trf) {
+							return trf.header.frame_id == parent_frame;
+						});
+		// Erase the elements from the original vector
+		unsorted_trf.transforms.erase(std::remove_if(unsorted_trf.transforms.begin(), unsorted_trf.transforms.end(),
+							[parent_frame](const auto & trf) {
+								return trf.header.frame_id == parent_frame;
+							}),
+					unsorted_trf.transforms.end());
+		// Get the new parent frame
+		parent_frame = sorted_trf.transforms[i].child_frame_id;
+		i++;
+	} while (unsorted_trf.transforms.size() > 0);
+
+	// Replace 'base_link' with robot and 'map' with world
+	std::for_each(sorted_trf.transforms.begin(), sorted_trf.transforms.end(), [](auto & trf) {
+		trf.header.frame_id = (trf.header.frame_id == "base_link") ? "robot" : trf.header.frame_id;
+		trf.header.frame_id = (trf.header.frame_id == "map") ? "world" : trf.header.frame_id;
+		trf.child_frame_id  = (trf.child_frame_id  == "base_link") ? "robot" : trf.child_frame_id;
+		trf.child_frame_id = (trf.child_frame_id  == "map") ? "world" : trf.child_frame_id;
+	});
+
+	for (auto trf : sorted_trf.transforms){
 		// Get the parent and child frames
 		std::string parent_frame = trf.header.frame_id;
 		std::string child_frame = trf.child_frame_id;
-
-		// Replace 'base_link' with robot
-		parent_frame = (parent_frame == "base_link") ? "robot" : parent_frame;
-		child_frame = (child_frame == "base_link") ? "robot" : child_frame;
-
-		// Replace 'map' with world
-		parent_frame = (parent_frame == "map") ? "world" : parent_frame;
-		child_frame = (child_frame == "map") ? "world" : child_frame;
 
 		// Get the nodes
 		std::optional<DSR::Node> parent_node = G_->get_node(parent_frame);
