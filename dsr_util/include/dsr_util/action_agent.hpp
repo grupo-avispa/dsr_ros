efa923875d66f5c7f915b302376e5a9cb11dae08
Alberto Tudela <ajtudela@gmail.com>
2025-02-11 11:22:23 +0100

    Fix uncrustify
    Signed-off-by: Alberto Tudela <ajtudela@gmail.com>

--- dsr_util/include/dsr_util/action_agent.hpp
+++ dsr_util/include/dsr_util/action_agent.hpp
@@ -194,7 +194,7 @@ protected:
       };
     send_goal_options.feedback_callback =
       [this](typename rclcpp_action::ClientGoalHandle<ActionT>::SharedPtr,
-        const std::shared_ptr<const typename ActionT::Feedback> feedback) {
+      const std::shared_ptr<const typename ActionT::Feedback> feedback) {
         feedback_ = feedback;
         on_feedback(feedback);
         update_dsr_when_feedback_received(feedback);
